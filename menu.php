8285ac357d2d7e23f6dde516ee93ee27bffc824d
Jeetix <jeetix@gmail.com>
2023-03-07 06:33:59 +0000

    Test editing from github codespace

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 <button class="btn headmenu" onclick="window.location.href='browse.php'">Browse all</button>
  -->
 <div class="menu-icon-bar">
-    <a class="menu-icon-bar-active" style="color: black;" href="./" title="Task List"><i class="bi bi-check2-square"></i> Task List</a>
+    <a class="menu-icon-bar-active" style="color: black;" href="./" title="Task List"><i class="bi bi-check2-square"></i> Task List.</a>
     <a href="./browse.php" title="Archive"><i class="bi bi-archive-fill"></i> </a>
     <!-- <a href="./add.php" title="Add"><i class="bi bi-plus-square-fill"></i> </a>
     <a href="./edit.php" title="Edit"><i class="bi bi-pencil-square"></i> </a>
